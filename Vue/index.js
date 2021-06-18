31c90bfd68d7d434abb7232cf6893b26885ff433
qinjian.jiang <qinjian.jiang@ersoft.cn>
2021-06-18 11:01:31 +0800

    feat: 数据代理

--- Vue/index.js
+++ Vue/index.js
@@ -1,1 +1,13 @@
-console.log(111)
+import { initState } from './init'
+const Vue = function (options) {
+    this._init(options)
+}
+
+Vue.prototype._init = function (options) {
+    const vm = this;
+    vm.$options = options;
+    initState(vm)
+};
+
+
+export default Vue
--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
-// import Vue from "Vue"
-console.log(222)
+import Vue from "Vue"
+const vm = new Vue({
+    el: '#app',
+    data () {
+        return {
+            title: '标题',
+            students: [
+                {
+                    name: '小明',
+                    class: '1'
+                },
+                {
+                    name: '小红',
+                    class: '2'
+                }
+            ],
+            teacher: {
+                'bb': {
+                    name: 'aa',
+                    use: 'cc'
+                },
+                'cc': {
+                    name: 'kk',
+                    use: 'll'
+                }
+            },
+            school: ['pp', 'uu']
+        }
+    }
+}) 
+
+console.log(vm.title)
